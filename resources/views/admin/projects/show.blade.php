2248d114cbb4a251e2900c15823a20b61890692f
Rivio98 <rivaldogjoni1998@gmail.com>
2024-10-12 14:55:32 +0200

    [UPD]Add show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -8,8 +8,15 @@
                 <p>{{ $project->slug }}</p>
                 <p>{{ $project->description }}</p>
                 <p>{{ $project->category ? $project->category->name : 'Nessuna Categoria' }}</p>
-                <img src="{{ asset('./storage/' . $project->project_image) }}" alt="{{ $project->name }}">
-            </div>
+                <p>Tecnologia usata:
+                    @forelse ($project->technologies as $technology)
+                        {{ $technology->name }}
+                    @empty
+                        Nessuna tecnologia associata
+                    @endempty
+            </p>
+            <img src="{{ asset('./storage/' . $project->project_image) }}" alt="{{ $project->name }}">
         </div>
     </div>
+</div>
 @endsection
